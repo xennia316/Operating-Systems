bfdf11d04b9751f94ac9f0bf825b6e771a5f00c1
eddybruv <edwinajong@gmail.com>
2022-01-21 15:50:34 +0100

    gcd and golden ratio added

--- Kernel Modules/Ex/simple.c
+++ Kernel Modules/Ex/simple.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/gcd.h>  //for gcd function
 
 /**
  * This function is called when the module is loaded.
@@ -8,6 +9,8 @@
 
 int simple_init(void){
     printk(KERN_INFO "Loading Kernel Module\n");
+    printk(KERN_INFO "Gcd: %lu\n", gcd(78, 9));
+    printk(KERN_INFO "Golden Ratio: %lu\n", GOLDEN_RATIO_PRIME);
     return 0;
 }
 
@@ -26,6 +29,6 @@ void simple_exit(void){
 module_init(simple_init);
 module_exit(simple_exit);
 
-MODULE_LICENSE("GPL");
+MODULE_INFO("GPL");
 MODULE_DESCRIPTION("Simple MOdule");
-MODULE_AUTHOR("SGG");
+MODULE_AUTHOR("eddybruv_ & zennia");
