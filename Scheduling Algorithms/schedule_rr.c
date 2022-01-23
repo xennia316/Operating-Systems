e71271e3847a9248c2c160702405ddfc348ba5ee
eddybruv <edwinajong@gmail.com>
2022-01-23 06:22:56 +0100

    Round robin

--- Scheduling Algorithms/schedule_rr.c
+++ Scheduling Algorithms/schedule_rr.c
@@ -2,77 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "task.h"
 #include "cpu.h"
 #include "schedulers.h"
 #include "list.h"
+#include "task.h"
 
-int task_id;
-struct node *head = NULL;
-struct node *copy_head;
-int t_burst_t;
-int shortest_time = 100000;
-int complete_time;
-int t_complete_time;
-int waiting_time;
-float avg_wait;
-float avg_turna;
-int count_round;
-int t_response;
-float avg_t_resp;
-// reverse list
+int task_id, waiting_time, task_completion_time, total_completion_time, shortest_time = 10000;
+int total_response_time, count, total_burst_time;
+float avg_waiting_time, avg_turnaround_time, avg_response_time;
+
+struct node *head_ref = NULL;
+struct node *head_copy;
+
 struct node *reverse(struct node *p_head)
 {
     struct node *r_head = NULL;
+
     while (p_head != NULL)
     {
         insert(&r_head, p_head->task);
         p_head = p_head->next;
     }
+
     return r_head;
 }
 
 void schedule()
 {
-    copy_head = malloc(sizeof(struct node));
-    copy_head = reverse(head);
-    head = copy_head;
+    head_copy = malloc(sizeof(struct node));
+    head_copy = reverse(head_ref);
+    head_ref = head_copy;
 
-    while (copy_head != NULL)
+    while (head_copy != NULL)
     {
-        count_round++;
-        while (head != NULL)
+        count++;
+        while (head_ref != NULL)
         {
-            if (count_round == 1)
+            if (count == 1)
+                total_response_time += task_completion_time;
+
+            if (head_ref->task->burst > 10)
             {
-                t_response += complete_time;
-            }
-            if (head->task->burst > 10)
-            {
-                run(head->task, 10);
-                head->task->burst = head->task->burst - 10;
-                complete_time += 10;
-            }
-            else if (head->task->burst > 0 && head->task->burst <= 10)
-            {
-                run(head->task, head->task->burst);
-                complete_time += head->task->burst;
-                head->task->burst = 0;
-                t_complete_time += complete_time;
-                delete (&copy_head, head->task);
+                run(head_ref->task, 10);
+                head_ref->task->burst = head_ref->task->burst - 10;
+                task_completion_time += 10;
             }
 
-            head = head->next;
+            else if (head_ref->task->burst > 0 && head_ref->task->burst <= 10)
+            {
+                run(head_ref->task, head_ref->task->burst);
+                task_completion_time += head_ref->task->burst;
+                head_ref->task->burst = 0;
+                total_completion_time += task_completion_time;
+                delete (&head_copy, head_ref->task);
+            }
+
+            head_ref = head_ref->next;
         }
-        head = copy_head;
+        head_ref = head_copy;
     }
-    waiting_time = t_complete_time - t_burst_t;
-    avg_wait = ((float)waiting_time) / (float)task_id;
-    avg_turna = ((float)t_complete_time) / (float)task_id;
-    avg_t_resp = ((float)t_response / (float)task_id);
-    printf("\nAverage waiting time = %.2f \n", avg_wait);
-    printf("Average turnaround time = %.2f \n", avg_turna);
-    printf("Average response time = %.2f \n", avg_t_resp);
+
+    waiting_time = total_completion_time - total_burst_time;
+    avg_waiting_time = ((float)waiting_time) / (float)task_id;
+    avg_turnaround_time = ((float)total_completion_time) / (float)task_id;
+    avg_response_time = ((float)total_response_time / (float)task_id);
+    printf("\nAverage waiting time = %.2f \n", avg_waiting_time);
+    printf("Average turnaround time = %.2f \n", avg_turnaround_time);
+    printf("Average response time = %.2f \n", avg_response_time);
 }
 
 void add(char *name, int priority, int burst)
@@ -84,6 +80,6 @@ void add(char *name, int priority, int burst)
     task->burst = burst;
     task->priority = priority;
     // calculate total burst time
-    t_burst_t += task->burst;
-    insert(&head, task);
+    total_burst_time += task->burst;
+    insert(&head_ref, task);
 }
